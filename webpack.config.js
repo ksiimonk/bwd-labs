1cdd657ff4bcfa74667c5efe5b4bd65fda2d204f
ksiimonk <kolotilinsemen@yandex.ru>
2024-11-18 13:16:11 +0400

    fix:LAB4-3 4-2 Исправление некоторых ошибок во многих пунктах

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,68 @@
-const path = require('path');
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-    },
-    output: {
-        filename: 'bundle.js', // Имя выходного файла сборки
-        path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
-        clean: true,
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/images', to: 'images' },
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/main.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'main.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/add.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'add.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/tasks.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'tasks.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/projects.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'projects.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/about.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'about.html'
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
-    },
+   entry: './src/index.js', // Точка входа для сборки проекта
+
+
+   output: {
+       filename: 'bundle.js', // Имя выходного файла сборки
+       path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+   },
+
+
+   module: {
+       rules: [
+           {
+               test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+               use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+           },
+       ],
+   },
+
+
+   plugins: [
+       new HtmlWebpackPlugin({
+           template: './src/tasks.html',
+           inject: true,
+           chunks: ['index'],
+           filename: 'tasks.html'
+       }),
+       new HtmlWebpackPlugin({
+        template: './src/add.html',
+        inject: true,
+        chunks: ['index'],
+        filename: 'add.html'
+    }),
+       new HtmlWebpackPlugin({
+        template: './src/main.html',
+        inject: true,
+        chunks: ['index'],
+        filename: 'main.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/projects.html',
+        inject: true,
+        chunks: ['index'],
+        filename: 'projects.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/about.html',
+        inject: true,
+        chunks: ['index'],
+        filename: 'about.html'
+    }),
+   ],
+
+
+   devServer: {
+       static: {
+           directory: path.join(__dirname, 'dist'), // Каталог для статики
+       },
+       open: true, // Автоматически открывать браузер
+   },
+
+
+   mode: 'development', // Режим сборки
 };
